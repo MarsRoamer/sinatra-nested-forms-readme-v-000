a272421d5edbf3dab633e3acf058440d3a2f0fe5
Jonathan Tiede <skysoldiermars@gmail.com>
2018-04-26 16:43:09 +0000

    Done.

---
+++ models/course.rb
@@ -0,0 +1,16 @@
+class Course
+  attr_reader :name, :topic
+
+  @@courses = []
+
+  def initialize(args)
+    @name = args[:name]
+    @topic = args[:topic]
+    @@courses << self
+  end
+
+  def self.all
+    @@courses
+  end
+
+end
