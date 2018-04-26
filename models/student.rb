04532dc28373446497540b9ffa0bf048c9ed1a2b
Jonathan Tiede <skysoldiermars@gmail.com>
2018-04-26 16:17:09 +0000

    automatically backed up by learn

--- models/student.rb
+++ models/student.rb
@@ -1,8 +1,16 @@
-class Student 
+class Student
+  attr_reader :name, :grade 
 
+  @@all = []
+  
+  def initialize(params)
+    @name = params[:name]
+    @grade = params[:grade]
+    @@all << self 
+  end 
 
-
-
-
+  def self.all 
+    @@all 
+  end
 
 end
